0a2fe812dffd6e15ad86b316c0525f6c4d3bf0f6
Daniel Stott <wdstott@hotmail.co.uk>
2025-05-05 20:31:06 +0100

    Add disko module to the configuration
    Include the disko module in `config.nix` to support disk management. This change also relocates `disko.config.nix` to the appropriate directory for better organization.

--- hosts/default/config.nix
+++ hosts/default/config.nix
@@ -14,6 +14,7 @@
     ./hardware.nix
     ./users.nix
     ./packages-fonts.nix
+    ../../disko/config.nix
     ../../modules/amd-drivers.nix
     ../../modules/nvidia-drivers.nix
     ../../modules/nvidia-prime-drivers.nix
